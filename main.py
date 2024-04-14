47f4a7b78d4a205aa36b83fa1b8ef46c09857e03
joshkiss <joshk7998@gmail.com>
2024-04-14 19:50:07 -0400

    main game loop and draw screen

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ pygame.init()
 FPS = 60
 
 WIDTH, HEIGHT = 800, 800
-ROWS = 4
-COLS = 4
+ROWS = 6
+COLS = 6
 
 RECT_HEIGHT = HEIGHT // ROWS 
 RECT_WIDTH = WIDTH // COLS 
@@ -19,8 +19,44 @@ BACKGROUND_COLOR = (205, 192, 180)
 FONT_COLOR = (119, 110, 101)
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption(2048)
+pygame.display.set_caption('2048')
 
 FONT = pygame.font.SysFont('comicsans', 60, bold=True)
 MOVE_VEL = 20 # pixels per second
 
+def draw_grid(window):
+    for row in range(1, ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+
+    for col in range(1, COLS):
+        x = col * RECT_WIDTH
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+
+    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+
+def draw(window):
+    window.fill(BACKGROUND_COLOR)
+
+    draw_grid(window)
+    
+    pygame.display.update()
+
+def main(window):
+    clock = pygame.time.Clock()
+    run = True 
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False 
+                break 
+
+        draw(window)
+
+    pygame.quit() 
+
+if __name__ == "__main__":
+    main(WINDOW)
